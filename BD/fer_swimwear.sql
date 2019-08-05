5d8fc1f1f00e4ae22576ccfc5f6656c282cf8453
Joaquin Pibernat <joaquin.pibernat@gmail.com>
2019-08-05 02:00:05 -0300

    Actualizacion de script sql con ultimas modificaciones en las tablas

--- BD/fer_swimwear.sql
+++ BD/fer_swimwear.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `fer_swimwear` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE  IF NOT EXISTS `fer_swimwear` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `fer_swimwear`;
 -- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
 --
@@ -68,6 +68,31 @@ LOCK TABLES `category` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `faq`
+--
+
+DROP TABLE IF EXISTS `faq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `faq` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `answer` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `faq`
+--
+
+LOCK TABLES `faq` WRITE;
+/*!40000 ALTER TABLE `faq` DISABLE KEYS */;
+INSERT INTO `faq` VALUES (1,'¿Cómo se cual es mi talla ideal?','Tu talle ideal es aquel con el que te sientas'),(2,'¿Cómo funciona el modelo unitalle?','Son modelos diseñados de manera de que puede '),(3,'¿Cómo puedo medirme?','Acá podrás encontrar las instrucciones para t'),(4,'¿Cómo comprar?','Ingresa: crea un usuario y contraseña, elige '),(5,'¿Realizan modelos Madre-Hija?','Estamos diseñando modelos Madre-Hija serán pa'),(6,'¿Cómo es el proceso para modelos para fechas especiales cómo: Despedida de Solteras, Navidad, etc?','Tenemos modelos y precios especiales por pedi'),(7,'¿Podré comprar un modelo diferente al que veo en el website?','Si deseas recrear un modelo que has visto en '),(8,'Acerca de las transparencias','Las telas que utilizamos son de alta calidad,'),(9,'¿Cómo puedo hacer para que el modelo que elegí dure mucho más tiempo?','En el blog podrás encontrar una guía de cuida');
+/*!40000 ALTER TABLE `faq` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `products`
 --
 
@@ -109,8 +134,9 @@ CREATE TABLE `users` (
   `password` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
   `avatar` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `users.id` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`idusers`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -119,7 +145,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'joaquin','','joaquin@dh.com','$2y$10$2grVNXe5u1oEHizMSk4wd.DaP0vTfjkEKr9cPx','5d421b7a28be0.png','1'),(2,'sabri','','sabri@dh.com','$2y$10$u1Tq4wxvOQ//Umx8YL4cRu3TxEjg0S7TAS5BD9KBl/nVdLbf8IiQm','5d421c3dad819.png','1'),(3,'coni','','coni@dh.com','$2y$10$DUn20yI26cDDwcJKeraEBOa1lklUTodVFWhUQCqoI24vf8uNS1rjW','5d4223c3851b2.png','1');
+INSERT INTO `users` VALUES (1,'joaquin','','joaquin@dh.com','$2y$10$2grVNXe5u1oEHizMSk4wd.DaP0vTfjkEKr9cPx','5d421b7a28be0.png','1',''),(2,'sabri','','sabri@dh.com','$2y$10$u1Tq4wxvOQ//Umx8YL4cRu3TxEjg0S7TAS5BD9KBl/nVdLbf8IiQm','5d421c3dad819.png','1',''),(3,'coni','','coni@dh.com','$2y$10$DUn20yI26cDDwcJKeraEBOa1lklUTodVFWhUQCqoI24vf8uNS1rjW','5d4223c3851b2.png','1',''),(4,'Mila','','mila@gmail.com','$2y$10$MsDItcpt7i/31hGhy5X3f.hyVGIzsU3m5NQhgGeKRUHMDQmyoSPYm','5d4647863a96c.jpg','1',''),(5,'Joaquin','Pibernat','joaquin@gmail.com','$2y$10$7DXMorLYSUpaAnwlIdqdTOiPe386oV530BEG5k13sYlcJCjFbgUxC','5d465ab2d5c8c.png','9','');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -132,4 +158,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-31 20:36:34
+-- Dump completed on 2019-08-05  1:59:19
